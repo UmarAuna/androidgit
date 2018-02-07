330d79eba9e7d2f5494674faee8173ec77602aa4
INTRUD£R\Umar Saidu Auna <umarmanofpeace@gmail.com>
2018-02-07 16:14:15 +0100

    Added Toast

--- app/src/main/java/com/gdgminna/myapplication/MainActivity.java
+++ app/src/main/java/com/gdgminna/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.gdgminna.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"hello android",Toast.LENGTH_SHORT).show();
     }
 }
